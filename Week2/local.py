bb907953e152fcb74eeff162a058e9ab8ad8baba
Josh Corn <josh@doubletake.design>
2025-02-06 12:40:48 -0500

    added week2

---
+++ Week2/local.py
@@ -0,0 +1,60 @@
+# this is a comment
+
+# underscores
+time_of_day = "morning"
+
+# camel case
+timeOfDay = "afternoon"
+currentWeather = "chilly"
+
+variableToSaveTheNameOfHisSister = "Martha"
+sisterName = "Martha"
+
+x = 36778
+count = 36778
+
+print("It is currently the ", timeOfDay, ".")
+
+print(f"The weather is {currentWeather}.")
+
+print("""
+An awesome approach
+to writing multi-line code
+is to use three quotes
+""")
+
+cat_art = """
+  /\_/\  
+ ( o.o ) 
+ > ^ <  
+
+   /\\_/\\  
+  ( o.o ) 
+   > ^ <  
+
+    /\\_/\\  
+   ( o.o ) 
+    > ^ <  
+
+     /\\_/\\  
+    ( o.o ) 
+     > ^ <  
+
+      /\\_/\\  
+     ( o.o ) 
+      > ^ <  
+
+        /\\_/\\  
+       ( o.o ) 
+        > ^ <  
+
+         /\\_/\\  
+        ( o.o ) 
+         > ^ <  
+
+          /\\_/\\  
+         ( o.o ) 
+          > ^ <  
+"""
+
+print(cat_art)
